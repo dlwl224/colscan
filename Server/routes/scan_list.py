9803258c837a85e46968dc7293e0ea1250f24179
dlwl224 <jw5194@naver.com>
2025-10-09 04:28:26 +0900

    서버/UI 완성

--- Server/routes/scan_list.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-scan_list_bp = Blueprint("scan_list", __name__)
-
-@scan_list_bp.route("/", methods=["GET"])
-def scan_list():
-    return render_template("scan_list.html")
